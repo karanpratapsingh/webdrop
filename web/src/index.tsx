7a1cbc3391a3e95c5c0225aed36ecee8c251f531
Karan Pratap Singh <synester.gates4@gmail.com>
2020-12-25 16:26:55 +0530

    refactor: remove CRA boilerplate

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './global/main.css';
+import reportWebVitals from './utils';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export function App(): React.ReactElement {
+  return (
+    <div>
+      <span>Webdrop</span>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ web/src/tests/App.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { App } from '../index';
+
+test('Renders learn react link', () => {
+  render(<App />);
+});
