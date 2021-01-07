1f6e42cb5bba81683ff741a99221b31f99f6233b
Karan Pratap Singh <synester.gates4@gmail.com>
2021-01-07 18:03:34 +0530

    refactor: dev env

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -4,9 +4,13 @@ export function getEnv(env: string): string {
   const foundEnv = process.env[env];
 
   if (!foundEnv) {
-    console.log(process.env);
     throw new Error(`ENV: ${env} not found`);
   }
 
   return foundEnv;
+}
+
+export function getAllowedOrigin(): string {
+  const developmentOrigin: string = 'http://localhost:3000';
+  return isDevelopment ? developmentOrigin : getEnv('REACT_APP_WEBDROP_PROXY');
 }
